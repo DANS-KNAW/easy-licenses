a17c8e1a3c5d88f3315996f15322f4a58b4f9561
jo-pol <jo-pol@users.noreply.github.com>
2019-11-22 13:37:41 +0100

    further refactoring and additional tests

--- src/test/scala/nl/knaw/dans/easy/licenses/TestFixture.scala
+++ src/test/scala/nl/knaw/dans/easy/licenses/TestFixture.scala
@@ -1,18 +1,38 @@
+/**
+ * Copyright (C) 2019 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.knaw.dans.easy.licenses
 
 import java.io.File
 
 import org.scalatest.{ FlatSpec, Inspectors, Matchers }
 
-trait TestFixture extends FlatSpec with Matchers with Inspectors  {
+trait TestFixture extends FlatSpec with Matchers with Inspectors {
+
   val LICENSES_DIR = "src/main/resources/licenses"
-  val extensionRegExp = ".[^.]+$"
-  val licenseFiles: List[String] = new File(LICENSES_DIR)
+
+  val documentFiles: List[String] = new File(LICENSES_DIR)
     .listFiles.filter(_.isFile).map(_.getName)
     .filterNot(n => n.endsWith("properties") || n.endsWith("json"))
     .toList
-  val baseFileNames: List[String] = licenseFiles
-    .map(_.replaceAll(extensionRegExp,""))
+
+  val documentBaseFileNames: List[String] = documentFiles
+    .map(stripExtension)
     .sortBy(identity).distinct
 
+  def stripExtension(fileName: String): String = {
+    fileName.replaceAll(".[^.]+$", "")
+  }
 }
